730a29b820de655637341ac7e07fab6ce421b489
heokyeongju <beewt@naver.com>
2022-07-14 15:37:31 +0900

    WiseSaying 클래스 추가

--- src/main/java/com/ll/exam/WiseSayingController.java
+++ src/main/java/com/ll/exam/WiseSayingController.java
@@ -14,6 +14,6 @@ public class WiseSayingController {
     }
 
     void modify(Rq rq) {
-
+        System.out.println("수정");
     }
 }
--- src/main/java/com/ll/exam/WiseSayingRepository.java
+++ src/main/java/com/ll/exam/WiseSayingRepository.java
@@ -1,4 +1,8 @@
 package com.ll.exam;
 
+import java.util.List;
+
 public class WiseSayingRepository {
+    List <WiseSaying> wiseSayings;
+    int wiseSayingLastId;
 }
